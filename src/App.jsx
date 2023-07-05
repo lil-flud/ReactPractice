ac55e60fc61a78247f87bbfa51788ddf910fac79
lil-flud <115752813+lil-flud@users.noreply.github.com>
2023-07-05 16:11:26 -0500

    Base component folders built.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
@@ -9,6 +14,12 @@ const App = () => {
     <Header />
     <Nav />
     <About />
+    <Experience />
+    {/* <Services /> */}
+    <Portfolio />
+    <Testimonials />
+    <Contact />
+    <Footer />
     </>
   )
 }
